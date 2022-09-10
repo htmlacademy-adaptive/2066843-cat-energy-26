736b6ab673b08e8ff5fc5fa5d3a4fb4138e249b1
Stanislav Prokofjev <Prokostas@gmail.com>
2022-09-10 12:58:04 +0300

    gulp fix

--- gulpfile.js
+++ gulpfile.js
@@ -19,7 +19,7 @@ import rename from 'gulp-rename';
 
 // Styles
 
- export const styles = () => {
+export const styles = () => {
   return gulp.src('source/less/style.less', { sourcemaps: true })
     .pipe(plumber())
     .pipe(less())
@@ -33,7 +33,7 @@ import rename from 'gulp-rename';
 }
 
 // HTML
- const html = () => {
+const html = () => {
   return gulp.src('source/*.html')
   .pipe(htmlmin({ collapseWhitespace: true }))
   .pipe(gulp.dest('build'));
@@ -53,27 +53,27 @@ const server = (done) => {
 }
 
 // Scripts
- const scripts = () => {
+const scripts = () => {
   return gulp.src('source/js/*.js')
   .pipe(terser())
   .pipe(gulp.dest('build/js'))
 }
 
 // Images
- const optimizeImages = () => {
+const optimizeImages = () => {
   return gulp.src('source/img/**/*.{jpg,png}')
   .pipe(squoosh())
   .pipe(gulp.dest('build/img'))
 }
 
- const copyImages = () => {
+const copyImages = () => {
   return gulp.src('source/img/**/*.{jpg,png}')
   .pipe(gulp.dest('build/img'))
 }
 
 
 //WebP
- const createWebP = () => {
+const createWebP = () => {
   return gulp.src('source/img/**/*.{jpg,png}')
   .pipe(squoosh({
     webp:{}
@@ -83,13 +83,13 @@ const server = (done) => {
 
 //SVG
 
- const svg = () =>
+const svg = () =>
   gulp.src(['source/img/*.svg', '!source/img/icon/*.svg'])
   .pipe(svgo())
   .pipe(gulp.dest('build/img'));
 
 
- const sprite = () => {
+const sprite = () => {
   return gulp.src('source/img/icon/*.svg')
   .pipe(svgo())
   .pipe(svgstore({
@@ -99,7 +99,7 @@ const server = (done) => {
   .pipe(gulp.dest('build/img'));
   }
 
- const copy = (done) => {
+const copy = (done) => {
   gulp.src([
     'source/fonts/*.{woff2,woff}'
   ], {
@@ -111,7 +111,7 @@ const server = (done) => {
 
  //Clean
 
- const clean = () => {
+const clean = () => {
   return del('build')
  };
 
@@ -139,7 +139,7 @@ const watcher = () => {
 
 //Build
 
- export const build = gulp.series(
+export const build = gulp.series(
   clean,
   copy,
   optimizeImages,
@@ -151,10 +151,10 @@ const watcher = () => {
     sprite,
     createWebP
   ),
- );
+);
 
 
- export default gulp.series(
+export default gulp.series(
   clean,
   copy,
   copyImages,
@@ -168,5 +168,5 @@ const watcher = () => {
   ),
   gulp.series(
     server,
-     watcher
+    watcher
   ));
